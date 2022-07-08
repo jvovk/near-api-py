06d1097afade014811af4b212c755c18b2c1d83f
Shockedshodan <46688871+Shockedshodan@users.noreply.github.com>
2022-07-08 20:51:03 +0100

    removed weird syntax

--- near_api/transactions.py
+++ near_api/transactions.py
@@ -256,7 +256,7 @@ tx_schema = dict(
 def sign_and_serialize_transaction(
         receiver_id: str,
         nonce: int,
-        actions: list[Action],
+        actions,
         block_hash: bytes,
         signer: 'near_api.signer.Signer'
 ) -> bytes:
